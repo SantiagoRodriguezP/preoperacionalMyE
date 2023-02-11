126e0df5269a3011773b6ac6f4d4e3ecc6ff0e45
SantiagoRodriguezP <santiago.rodriguez@sinco.com.co>
2023-02-10 19:42:52 -0500

    estilos home

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, AppBar,Toolbar,Typography, IconButton} from "@mui/material";
+import {Box, AppBar,Toolbar,Typography, IconButton,Autocomplete} from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import Card from "@mui/material/Card";
 import {CardContent,CardMedia, TextField} from "@mui/material";
@@ -9,14 +9,18 @@ import { Stack,InputAdornment } from '@mui/material';
 import Image from 'next/image';
 
  const HomePage = () => {
+  const options = [
+    { label: 'The Godfather', id: 1 },
+    { label: 'Pulp Fiction', id: 2 },
+  ];
   return (
-    <Stack width='100vw' height='100vh'>
+    <Stack width='98vw' height='100vh'>
       <AppBar position="static" color="primary">
         <Toolbar sx={{backgroundColor:'#003972'}}>
           <Box sx={{flexGrow:1}}>
             <IconButton color="inherit">
               <EventIcon/>
-              <Typography >11/01/2023</Typography>
+              <Typography pl={2}>11/01/2023</Typography>
             </IconButton>
           </Box>
           <Typography variant="h6">
@@ -31,20 +35,22 @@ import Image from 'next/image';
           </Typography>
         </Toolbar>
       </AppBar>
-      <Card variant="outlined"  >
+      <Stack>
+      <Card>
         <CardMedia 
           image="/equipo.svg"
-          title="Equipo">
+          title="Equipo" >
          <CardContent sx={{width:430, height:227, justifyContent:'center', display:'flex', flexDirection:'column'}} >
-          <Typography variant="h3" sx={{fontSize:28, color:'white'}}>Hola Carlos</Typography>
+          <Typography variant="h3" sx={{fontSize:28, color:'white'}}>Hola Santiago</Typography>
           <Typography variant="h4" sx={{fontSize:20,color:'white'}}>¿Con que equipo quieres trabajar hoy?</Typography>
         </CardContent> 
         </CardMedia>
       </Card>
+      </Stack>
       <Stack justifyContent='center' alignItems='center'>
-        <Card sx={{position:'absolute', top:270, bottom:0, width:374, height:82}} >
+        <Card sx={{position:'absolute', top:230, bottom:100, width:374, height:82}} >
           <CardContent>
-            <TextField
+            {/* <TextField
               id="buscar"
               label="Buscar equipo"
               fullWidth
@@ -53,11 +59,19 @@ import Image from 'next/image';
                   <SearchIcon/>
                 </InputAdornment>
               )}}
-            />  
+            />   */}
+            <Autocomplete
+              disablePortal
+              id="combo-box-demo"
+              options={options}
+              sx={{ width: 300 }}
+              renderInput={(params) => <TextField {...params} label="Movie" />}
+              fullWidth
+            />
           </CardContent>
         </Card>
       </Stack>
-      <Stack justifyContent='center' alignItems='center'>
+      <Stack justifyContent='center' alignItems='center' width="98vw" height="100vh">
         <Image 
           src="./ElegirEquipo.svg"
           alt="default state"
